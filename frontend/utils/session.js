beae60b0bf1ef244d24da9a79667c9abd82067a0
vigism <rushilvig@gmail.com>
2019-06-28 09:32:02 -0700

    Create actions for user authentication

--- frontend/utils/session.js
+++ frontend/utils/session.js
@@ -4,4 +4,19 @@ export const postUser = user => (
     method: 'post',
     data: {user}
   })
-)
+);
+
+export const postSession = user => (
+  $.ajax({
+    url: '/api/session',
+    method:'post',
+    data: {user}
+  })
+);
+
+export const deleteSession = () => (
+  $.ajax({
+    url: '/api/session',
+    method: 'delete'
+  })
+);
